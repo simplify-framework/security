3989315041d32c861f538c5358fbdeb7290cca9e
Duong Dinh Cuong <cuong3ihut@gmail.com>
2020-06-22 17:17:26 +0200

    Update entrypoint.js

--- entrypoint.js
+++ entrypoint.js
@@ -39,7 +39,7 @@ var argv = require('yargs')
     .describe('region', 'AWS Specific Region')
     .alias('r', 'region')
     .default('region', 'eu-west-1')
-	.string('simple')
+    .string('simple')
     .alias('s', 'simple')
     .describe('simple', 'Simple metric view')
     .demandOption(['i'])
@@ -57,7 +57,7 @@ var headers = files[lineIndex++]
 
 function getSnapshotFromFile(snapshotPath) {
     simplify.consoleWithMessage(opName, `${cmdOPS} Snapshot from ${snapshotPath}`)
-    if (fs.existsSync(snapshotPath)) {    
+    if (fs.existsSync(snapshotPath)) {
         return JSON.parse(fs.readFileSync(snapshotPath).toString())
     } else {
         return undefined
@@ -138,7 +138,7 @@ function analyseOrPatch(args) {
                 }
             } else if (cmdOPS === 'CHECK') {
                 if (secureFunction) {
-                    simplify.consoleWithMessage(opName, `${cmdOPS} ${functionInfo.FunctionName} : ${functionInfo.KMSKeyArn == customKmsArn ? (functionInfo.KMSKeyArn ? `Has already configure with KMS Custom KeyId \x1b[32m[GOOD]\x1b[0m` : `Provide KMS Custom KeyId to setup secure function! \x1b[33m (WARN) \x1b[0m`) : ( customKmsArn ? `Has KMS Custom KeyId but not set! \x1b[33m (WARN) \x1b[0m`: `Missing KMS Custom KeyId \x1b[33m (WARN) \x1b[0m`)}`)
+                    simplify.consoleWithMessage(opName, `${cmdOPS} ${functionInfo.FunctionName} : ${functionInfo.KMSKeyArn == customKmsArn ? (functionInfo.KMSKeyArn ? `Has already configure with KMS Custom KeyId \x1b[32m[GOOD]\x1b[0m` : `Provide KMS Custom KeyId to setup secure function! \x1b[33m (WARN) \x1b[0m`) : (customKmsArn ? `Has KMS Custom KeyId but not set! \x1b[33m (WARN) \x1b[0m` : `Missing KMS Custom KeyId \x1b[33m (WARN) \x1b[0m`)}`)
                 } else {
                     simplify.consoleWithMessage(opName, `${cmdOPS} ${functionInfo.FunctionName} : ${functionInfo.KMSKeyArn == customKmsArn ? `Not require to use KMS Custom KeyId \x1b[32m[GOOD]\x1b[0m` : `Not matching KMS Custom KeyIds \x1b[33m (WARN) \x1b[0m`}`)
                 }
@@ -160,7 +160,7 @@ function analyseOrPatch(args) {
                     simplify.consoleWithMessage(opName, `${cmdOPS} ${functionInfo.FunctionName} : Configured secure logs with ${logRetention} days! \x1b[32m (OK) \x1b[0m`)
                     if (secureFunction) {
                         simplify.consoleWithMessage(opName, `${cmdOPS} ${functionInfo.FunctionName} : Configured with KMS Custom KeyId \x1b[32m (OK) \x1b[0m`)
-						args.customKmsArn = functionInfo.KMSKeyArn
+                        args.customKmsArn = functionInfo.KMSKeyArn
                     }
                     resolve(args)
                 }).catch(function (err) {
@@ -168,7 +168,7 @@ function analyseOrPatch(args) {
                 })
             } else if (cmdOPS === 'CHECK') {
                 if (secureFunction) {
-                    simplify.consoleWithMessage(opName, `${cmdOPS} ${functionInfo.FunctionName} : ${functionInfo.KMSKeyArn == customKmsArn ? (functionInfo.KMSKeyArn ? `Has already configure with KMS Custom KeyId \x1b[32m[GOOD]\x1b[0m` : `Provide KMS Custom KeyId to setup secure function! \x1b[33m (WARN) \x1b[0m`) : ( customKmsArn ? `Has KMS Custom KeyId but not set! \x1b[33m (WARN) \x1b[0m`: `Has configured with KMS but Custom KeyId input is not provided. \x1b[33m (WARN) \x1b[0m`)}`)
+                    simplify.consoleWithMessage(opName, `${cmdOPS} ${functionInfo.FunctionName} : ${functionInfo.KMSKeyArn == customKmsArn ? (functionInfo.KMSKeyArn ? `Has already configure with KMS Custom KeyId \x1b[32m[GOOD]\x1b[0m` : `Provide KMS Custom KeyId to setup secure function! \x1b[33m (WARN) \x1b[0m`) : (customKmsArn ? `Has KMS Custom KeyId but not set! \x1b[33m (WARN) \x1b[0m` : `Has configured with KMS but Custom KeyId input is not provided. \x1b[33m (WARN) \x1b[0m`)}`)
                 } else {
                     simplify.consoleWithMessage(opName, `${cmdOPS} ${functionInfo.FunctionName} : ${functionInfo.KMSKeyArn == customKmsArn ? `Has already configure with Custom KMS KeyId \x1b[32m[GOOD]\x1b[0m` : `Not matching KMS Custom KeyIds \x1b[33m (WARN) \x1b[0m`}`)
                 }
@@ -188,15 +188,15 @@ const secOpsFunctions = function (files, callback) {
             const functionName = parts[2]
             const functionVersion = parts[3] || undefined
             const logRetention = parts[4] || 90
-			const accountId = parts[1]
-			if (accountId.indexOf('E')!==-1) {
-				simplify.consoleWithMessage(opName, `${cmdOPS} ${functionName} Invalid AccountId number format \x1b[31m (ERROR) \x1b[0m`)
-			}
+            const accountId = parts[1]
+            if (accountId.indexOf('E') !== -1) {
+                simplify.consoleWithMessage(opName, `${cmdOPS} ${functionName} Invalid AccountId number format \x1b[31m (ERROR) \x1b[0m`)
+            }
             const customKmsArn = parts[5] ? `arn:aws:kms:${parts[0]}:${accountId}:key/${parts[5]}` : null
             const secureFunction = JSON.parse((parts[6] || 'false').toLowerCase())
             const secureLog = JSON.parse((parts[7] || 'false').toLowerCase())
             if (cmdOPS === 'METRIC') {
-                funcList.push({ functionInfo: { FunctionName: `${functionName}`} })
+                funcList.push({ functionInfo: { FunctionName: `${functionName}` } })
                 if (lineIndex >= files.length) {
                     callback && callback(funcList)
                 } else {
@@ -253,34 +253,41 @@ try {
                         let thisDate = new Date()
                         thisDate.setMinutes(0)
                         let mData = {}
-                        metrics.MetricDataResults.map(m => {
+                        let totalValues = {}
+                        const table = new utilities.PrintTable()
+                        for (let mi = 0; mi < metrics.MetricDataResults.length; mi++) {
+                            let data = {}
+                            const m = metrics.MetricDataResults[mi]
                             let timeValue = thisDate.toISOString()
                             const labelValue = `${m.Label}`
-                            const totalValue = parseFloat(m.Values.reduce((count, x) => count + x, 0)).toFixed(labelValue === 'Duration' ? 2: 0)
-                            const functionName = functionList[m.Id.split('_')[1]].functionInfo.FunctionName
+                            const totalPeriodValue = parseFloat(m.Values.reduce((count, x) => count + x, 0))
+                            if (!totalValues[labelValue]) totalValues[labelValue] = 0
+                            if (labelValue === 'Duration' || labelValue === 'Concurrency') {
+                                totalValues[labelValue] = Math.max(totalValues[labelValue], ...m.Values)
+                                totalValues[labelValue] = (labelValue === 'Duration' ? totalValues[labelValue].toFixed(2) : totalValues[labelValue])
+                            } else {
+                                totalValues[labelValue] += totalPeriodValue
+                            }
+                            const functionId = m.Id.split('_')[1]
+                            const functionName = functionList[functionId].functionInfo.FunctionName
                             if (!m.Values.length) {
                                 m.Values.push('0')
                                 m.Timestamps.push(timeValue)
                             }
-                            for (let i = 0; i < m.Values.length; i++) {
-                                const periodTimeValue= new Date(m.Timestamps[i]).toISOString()
-                                if (!mData[periodTimeValue]) {
-                                    mData[periodTimeValue] = { }
-                                    mData[periodTimeValue][`DateTime (${lastHours} hours ago)`] = periodTimeValue
-                                }
-                                let data = {}
-                                const textValue = parseFloat(m.Values[i] || '0').toFixed(labelValue === 'Duration' ? 2: 0)
-                                data[labelValue] =  labelValue === 'Duration' ? `${textValue} avg` : `${textValue} / ${totalValue}`
-                                mData[periodTimeValue] = { 'Function': functionName, ...mData[periodTimeValue], ...data }
-                            }
-                        })
-                        utilities.printTableWithJSON(Object.keys(mData).map(k => mData[k]))
+                            data[labelValue] = (labelValue === 'Duration' || labelValue === 'Concurrency' ? Math.max(...m.Values) : totalPeriodValue)
+                            data[labelValue] = (labelValue === 'Duration' ? data[labelValue].toFixed(2) : data[labelValue])
+                            mData[functionId] = { 'Function': functionName, ...mData[functionId], ...data }
+                        }
+                        let dataRows = Object.keys(mData).map(k => mData[k])
+                        table.addRows(dataRows)
+                        table.addRow({ 'Function': `Statistics in ${lastHours} hours`, ...totalValues}, { color: 'white_bold' })
+                        table.printTable()
                     }).catch(err => simplify.consoleWithMessage(`${err}`))
                 } else if (cmdOPS === 'VERIFY') {
-					let isSimpleView = false
-					if (typeof argv.simple !== 'undefined') {
-						isSimpleView = true
-					}
+                    let isSimpleView = false
+                    if (typeof argv.simple !== 'undefined') {
+                        isSimpleView = true
+                    }
                     const snapshotList = getSnapshotFromFile(path.join(__dirname, argv.output, `${argv.baseline || '$LATEST'}.json`))
                     const outputTable = functionList.map(func => {
                         const snapshot = snapshotList ? snapshotList.find(f => f.FunctionName === func.functionInfo.FunctionName) : { Layers: [] }
@@ -291,21 +298,21 @@ try {
                                 areLayersValid = false
                             }
                         })
-						const basicView = {
+                        const basicView = {
                             FunctionName: func.functionInfo.FunctionName.truncateLeft(20),
                             CodeSha256: `${func.functionInfo.CodeSha256.truncateLeft(5)} (${func.functionInfo.CodeSha256 === snapshot.CodeSha256 ? 'OK' : 'NOK'})`,
                             Layers: `${func.Layers.length} (${areLayersValid ? 'OK' : 'NOK'})`,
-                            LogRetention: `${func.LogGroup.retentionInDays || '-'} / ${func.logRetention} (${func.LogGroup.retentionInDays == func.logRetention ? 'OK': 'PATCH'})`,
-                            EncryptionKey: (func.customKmsArn ? `KMS ${func.functionInfo.KMSKeyArn === func.customKmsArn ? '(OK)' : '(PATCH)'}`: `${func.functionInfo.KMSKeyArn ? 'KMS': 'Default'} ${func.functionInfo.KMSKeyArn === func.customKmsArn ? '(OK)' : '(PATCH)'}`).truncateLeft(13),
+                            LogRetention: `${func.LogGroup.retentionInDays || '-'} / ${func.logRetention} (${func.LogGroup.retentionInDays == func.logRetention ? 'OK' : 'PATCH'})`,
+                            EncryptionKey: (func.customKmsArn ? `KMS ${func.functionInfo.KMSKeyArn === func.customKmsArn ? '(OK)' : '(PATCH)'}` : `${func.functionInfo.KMSKeyArn ? 'KMS' : 'Default'} ${func.functionInfo.KMSKeyArn === func.customKmsArn ? '(OK)' : '(PATCH)'}`).truncateLeft(13),
                             SecureFunction: func.secureFunction ? (func.functionInfo.KMSKeyArn ? 'YES (OK)' : 'YES (PATCH)') : (func.functionInfo.KMSKeyArn ? 'NO (PATCH)' : 'NO (OK)'),
                             SecureLog: func.secureLog ? (func.LogGroup.kmsKeyId ? 'YES (OK)' : 'YES (PATCH)') : (func.LogGroup.kmsKeyId ? 'NO (PATCH)' : 'NO (OK)')
                         }
-						const extendedView = {
-							LastModified: new Date(func.functionInfo.LastModified).toISOString(),
-							State: func.functionInfo.State,
-							CodeSize: `${func.functionInfo.CodeSize} bytes`,
-							Timeout: `${func.functionInfo.Timeout} secs`
-						}
+                        const extendedView = {
+                            LastModified: new Date(func.functionInfo.LastModified).toISOString(),
+                            State: func.functionInfo.State,
+                            CodeSize: `${func.functionInfo.CodeSize} bytes`,
+                            Timeout: `${func.functionInfo.Timeout} secs`
+                        }
                         return isSimpleView ? basicView : { ...basicView, ...extendedView }
                     })
                     utilities.printTableWithJSON(outputTable)
